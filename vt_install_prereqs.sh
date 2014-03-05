38cf0e8a77a353ce3cce125c93c3fa18eba6bb5e
Chris Lee <chris@the-eye.(none)>
2014-03-04 17:19:19 -0800

    *** empty log message ***

--- vt_install_prereqs.sh
+++ vt_install_prereqs.sh
@@ -1,7 +1,11 @@
 #! /usr/bin/env bash
+# For Mint 15: deb http://ubuntu.media.mit.edu/ubuntu/ raring main universe
+# For Ubuntu 12 LTS: deb http://ubuntu.media.mit.edu/ubuntu/ precise main universe
+#If one part stalls, try commenting it out.
+echo 'deb http://ubuntu.media.mit.edu/ubuntu/ precise main universe' >> /etc/apt/sources.list
+sudo apt-get update &&
 
-sudo apt-get update
-
+apt-get install -y git &&
 sudo apt-get install -y python-pip &&
 sudo apt-get install -y build-essential &&
 
@@ -13,10 +17,12 @@ sudo apt-get install -y libxcb1 libxcb1-dev libx11-xcb1 libx11-xcb-dev libxcb-ke
 
 sudo apt-get install -y mongodb mongodb-dev &&
 sudo apt-get install -y python-dev &&
-#sudo pip install -U numpy &&
-sudo apt-get install -y python-numpy &&
+sudo apt-get install -y -f python-numpy &&
+sudo pip install -U numpy &&
 
 sudo pip install -U networkx &&
+
+apt-get install -y -f python-pymongo &&
 
 sudo apt-get install -y libqt4-opengl &&
 sudo apt-get install -y python-qt4-gl &&
@@ -32,7 +38,6 @@ sudo apt-get install -y python-suds &&
 sudo apt-get install -y python-support 
 
 sudo apt-get install python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose -y
-sudo pip install pymongo
 
 #This command will initialize the DB
 #mongorestore --host localhost --db PW_2012_09_02_09_03_35 --directoryperdb {data-dir} --drop 
